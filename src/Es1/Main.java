a6ac259a21db4da28630bf1a3145a221b674a9e6
Raviolz <gi.ravioli@gmail.com>
2026-03-16 16:24:42 +0100

    es2.Interi

--- src/Es1/Main.java
+++ src/Es1/Main.java
@@ -1,3 +1,5 @@
+package Es1;
+
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
---
+++ src/Es2/Main.java
@@ -0,0 +1,26 @@
+package Es2;
+
+import java.util.Scanner;
+
+public class Main {
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        try {
+            System.out.println("Per sapere quanti km al litro inserisci quanti km hai fatto");
+            int km = Integer.parseInt(scanner.nextLine());
+            System.out.println("Inserisci quanti litri hai consumato");
+            int litri = Integer.parseInt(scanner.nextLine());
+
+            System.out.println(km / litri);
+        } catch (ArithmeticException e) {
+            System.out.println("operazione non possibile");
+        } catch (NumberFormatException e) {
+
+            System.out.println("Devi inserire un numero");
+
+            ;
+        }
+    }
+}
