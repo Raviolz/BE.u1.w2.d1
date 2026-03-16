85391f2099346c38cb31a0903a1c0e74cee894d4
Raviolz <gi.ravioli@gmail.com>
2026-03-16 16:00:43 +0100

    E.BuondE.Format.ES1done

--- src/Main.java
+++ src/Main.java
@@ -17,24 +17,58 @@ public class Main {
 
         System.out.println(Arrays.toString(numbArr));
 
-        System.out.println("Scegli da 1 a 5 la posizione da modificare (0 per uscire)");
-        int position = Integer.parseInt(scanner.nextLine());
+//        System.out.println("Scegli da 1 a 5 la posizione da selezionare nell array, 0 per terminare");
+//        int position = Integer.parseInt(scanner.nextLine());
+//
+//        while (position != 0) {
+//
+//            try {
+//
+//                System.out.println("Scegli un numero da inserire");
+//                int numbToSwitch = Integer.parseInt(scanner.nextLine());
+//
+//                numbArr[position - 1] = numbToSwitch;
+//
+//                System.out.println(Arrays.toString(numbArr));
+//
+//            } catch (ArrayIndexOutOfBoundsException e) {
+//                System.out.println("Posizione non valida");
+//            }
+//
+//            System.out.println("Scegli da 1 a 5 la posizione da selezionare nell array, 0 per terminare");
+//            position = Integer.parseInt(scanner.nextLine());
+//        }
 
-        while (position != 0) {
+        // COSI TI DICE CHE NON VA BENE SOLO DOPO AVER MESSO IL NUMERO ANCHE SE SA GIA CHE LA POSZIONE E' SBAGLIATA
 
-            System.out.println("Scegli un numero da inserire");
-            int numbToSwitch = Integer.parseInt(scanner.nextLine());
 
-            numbArr[position - 1] = numbToSwitch;
+        while (true) {
 
-            System.out.println(Arrays.toString(numbArr));
 
-            System.out.println("Scegli da 1 a 5 la posizione da modificare (0 per uscire)");
-            position = Integer.parseInt(scanner.nextLine());
+            try {
+                System.out.println("Scegli da 1 a 5 la posizione da selezionare nell'array, 0 per terminare");
+                int position = Integer.parseInt(scanner.nextLine());
+
+                if (position == 0) {
+                    break;
+                }
+                int test = numbArr[position - 1]; // controllo prima di usarlo in modo da andare subito nel catch senza chiedere altro // SI BLOCCA GIA QUI SE OUT
+
+                System.out.println("Scegli un numero da inserire");
+                int numbToSwitch = Integer.parseInt(scanner.nextLine());
+
+                numbArr[position - 1] = numbToSwitch;
+                System.out.println(Arrays.toString(numbArr));
+
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Posizione non valida, riprova");
+            } catch (NumberFormatException e) {
+
+                System.out.println("Devi inserire un numero");
+
+            }
         }
 
         System.out.println("Programma terminato");
     }
 }
-
-
